b908958ee84b7e838d01d86378e564da2a17cf83
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2023-06-23 23:16:13 +0100

    Ep-7 Routing - react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header'
 import Body from './components/Body';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,4 +17,20 @@ const AppLayout = () => {
         </div>
     )
 }
-root.render(<AppLayout/>);
+
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout/>,
+        errorElement:<Error />
+    },
+    {
+        path: '/about-us',
+        element:<About />
+    },
+    {
+        path: '/contact-us',
+        element: <Contact/>
+    }
+]);
+root.render(<RouterProvider  router={appRouter}/>);
---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return (<div className="about-us">
+        <h1>About Us Page</h1>
+    </div>)
+}
+
+export default About;
